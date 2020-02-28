87c7edf5edc23ad30bcafbb89012f4a45f876773
Adam Niehenke <a.niehenke@setonhill.edu>
2020-02-27 19:02:50 -0500

    Initial Commit

---
+++ Homework 4 Niehenke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 4 Niehenke
+//
+//  Created by Adam Niehenke on 2/27/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
